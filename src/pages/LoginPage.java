88886dda9c87c777e0a99a65df259f595f05e89c
Sripriya <sripriyamurthy07@gmail.com>
2022-05-25 14:01:20 +0530

    Modified page classes

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,11 @@
 package pages;
 
 public class LoginPage {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello LP");
+	
+		System.out.println("Hello LP");
+	}
+	
 }
--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -3,5 +3,7 @@ package pages;
 public class SearchPage {
 public void search() {
 	System.out.println("This is search");
+	
+	System.out.println("Hello LP");
 }
 }
